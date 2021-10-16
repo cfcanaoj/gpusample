5336bfc364e2522553cf7a7fa4ea3b880eb55d02
Tomoya Takiwaki <takiwaki.tomoya@nao.ac.jp>
2021-10-16 15:37:19 +0900

    update

---
+++ fortran_mpi/pbs_sample1_cpu.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+#PBS -q dgx-full
+#PBS -N job_sample1
+#PBS -l walltime=00:00:60
+#PBS -l select=1:ngpus=1:ncpus=2
+module load nvhpc
+cd $PBS_O_WORKDIR
+mpiexec -n 2 sample1.cpux
---
+++ fortran_mpi/sample1_cpu.f90
@@ -0,0 +1,78 @@
+module mpisetup
+  implicit none
+  include "mpif.h"
+!==========================================
+! Setup for MPI
+!==========================================
+  integer, parameter :: mreq  = 300
+  integer :: stat(MPI_STATUS_SIZE,mreq)
+  integer :: req(mreq)
+  integer :: nprocs_w,myid_w
+  integer :: ierr
+contains
+  subroutine InitiaizeMPI
+   implicit none
+!==========================================
+! Initialize MPI
+!==========================================
+  call MPI_INIT( ierr )
+  call MPI_COMM_SIZE( MPI_COMM_WORLD, nprocs_w, ierr )
+  call MPI_COMM_RANK( MPI_COMM_WORLD, myid_w  , ierr )
+  end subroutine InitiaizeMPI
+end module mpisetup
+
+program main
+  use mpisetup
+  implicit none
+!==========================================
+  integer, parameter:: SIZE=128
+  integer:: i
+  real(4),dimension(SIZE):: h_InA,h_InB,h_Out
+  integer,dimension(2) :: seed
+  real(8)::rnum
+  integer,parameter::unitout=121
+  character*50::fileout
+  nprocs_w=2
+  call InitiaizeMPI
+
+  write(fileout,"(a8,i3.3,a7)") "sample1-",myid_w,".gpulog"
+  open (unit=unitout,file=fileout,status='replace',form='formatted')
+  write(unitout,*) "CPU:\n"
+
+  seed(1)=1
+  seed(2)=1+myid_w*nprocs_w
+  call random_seed(PUT=seed(1:2))
+  
+  do i=1, SIZE
+     call random_number(rnum)
+     h_InA(i) = rnum
+  enddo
+  do i=1, SIZE
+     call random_number(rnum)
+     h_InB(i) = rnum
+  enddo
+
+  write(6,*)"InA: "
+  do i=1,SIZE
+     write(unitout,"(1x,f4.2)",advance='no') h_InA(i)
+  enddo
+  write(unitout,*) ""
+
+  write(unitout,*)"InB: "
+  do i=1,SIZE
+     write(unitout,"(1x,f4.2)",advance='no') h_InB(i)
+  enddo
+  write(unitout,*) ""
+  
+
+  do i=1, SIZE
+    h_Out(i) = h_InA(i)+ h_InB(i)
+ enddo
+ 
+ write(unitout,*)"Out: "
+  do i=1,SIZE
+     write(unitout,"(1x,f4.2)",advance='no') h_Out(i)
+  enddo
+  write(unitout,*) ""
+  
+end program
