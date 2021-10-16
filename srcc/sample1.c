f41ffc0f6e494c106bc84a65c74b2f5d5d26d7c8
Tomoya Takiwaki <takiwaki.tomoya@nao.ac.jp>
2021-10-16 12:13:53 +0900

    update

--- srcc/sample1.c
+++ srcc/sample1.c
@@ -5,7 +5,7 @@
 
 int main(int argc, char**argv){
   int i;
-  printf("GPU:\n");
+  printf("CPU:\n");
   srand(0);
   
   float *h_InA, *h_InB;
@@ -15,5 +15,16 @@ int main(int argc, char**argv){
   for(i=0;i<SIZE;i++) h_InA[i] = (float)(rand()%10)/10.0f;
   for(i=0;i<SIZE;i++) h_InB[i] = (float)(rand()%10)/10.0f; 
 
+  printf("InA: "); for(i=0;i<SIZE;i++) printf(" %.2f",h_InA[i]); printf("\n");
+  printf("InB: "); for(i=0;i<SIZE;i++) printf(" %.2f",h_InB[i]); printf("\n");
+
+  float *h_Out;
+  h_Out = (float*)malloc(sizeof(float)*SIZE);
+
+  for(i=0;i<SIZE;i++) 
+    h_Out[i] = h_InA[i]+ h_InB[i];
+  
+  printf("Out: "); for(i=0;i<SIZE;i++) printf(" %.2f",h_Out[i]); printf("\n");
+  
   return 0;
 }
--- srcc/sample1.cu
+++ srcc/sample1.cu
@@ -3,6 +3,7 @@
 
 #define SIZE 256
 
+/* define kernel functions */
 __global__ void arrayadd(float *fOut, float *fInA, float *fInB){
   int id = threadIdx.x + blockIdx.x  * blockDim.x;
   fOut[id] = fInA[id] +fInB[id];
@@ -14,13 +15,38 @@ int main(int argc, char**argv){
   srand(0);
 
   cudaSetDevice(0);
-  
-  float *h_InA, *h_InB;
+  /* variables in host */
+  float *h_InA, *h_InB, *h_Out;
   h_InA = (float*)malloc(sizeof(float)*SIZE);
   h_InB = (float*)malloc(sizeof(float)*SIZE);
+  h_Out = (float*)malloc(sizeof(float)*SIZE);
 
+  /* initialize */
   for(i=0;i<SIZE;i++) h_InA[i] = (float)(rand()%10)/10.0f;
   for(i=0;i<SIZE;i++) h_InB[i] = (float)(rand()%10)/10.0f; 
 
+  /* confirm */
+  printf("InA: "); for(i=0;i<SIZE;i++) printf(" %.2f",h_InA[i]); printf("\n");
+  printf("InB: "); for(i=0;i<SIZE;i++) printf(" %.2f",h_InB[i]); printf("\n");
+
+  /* variables in device */
+  float *d_InA, *d_InB, *d_Out;
+  cudaMalloc((void**)&d_InA, sizeof(float)*SIZE);
+  cudaMalloc((void**)&d_InB, sizeof(float)*SIZE);
+  cudaMalloc((void**)&d_Out, sizeof(float)*SIZE);
+  
+  /* transfer from host to device */
+  cudaMemcpy(d_InA, h_InA, sizeof(float)*SIZE, cudaMemcpyHostToDevice);
+  cudaMemcpy(d_InB, h_InB, sizeof(float)*SIZE, cudaMemcpyHostToDevice);
+
+  /* call kernel functions */
+  arrayadd<<< 1,1 >>> (d_Out,d_InA, d_InB);    
+
+  /* transfer from device to host */
+  cudaMemcpy(h_Out, d_Out, sizeof(float)*SIZE, cudaMemcpyDeviceToHost);
+ 
+  /* confirm */
+  printf("Out: "); for(i=0;i<SIZE;i++) printf(" %.2f",h_Out[i]); printf("\n");
   return 0;
 }
+
