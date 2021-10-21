340f1c3a8d52d3682f99619d09c609fb023a0001
Tomoya Takiwaki <takiwaki.tomoya@nao.ac.jp>
2021-10-21 10:16:02 +0900

    update

--- cpp/sample1.cpp
+++ cpp/sample1.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cstdlib>
+#include <vector>
 
 #define SIZE 256
 
@@ -7,19 +8,13 @@ int main(int argc, char**argv){
   int i;
   printf("CPU:\n");
   srand(0);
-  
-  float *h_InA, *h_InB;
-  h_InA = (float*)malloc(sizeof(float)*SIZE);
-  h_InB = (float*)malloc(sizeof(float)*SIZE);
+  std::vector<float> h_InA(SIZE),h_InB(SIZE),h_Out(SIZE);
 
   for(i=0;i<SIZE;i++) h_InA[i] = (float)(rand()%10)/10.0f;
   for(i=0;i<SIZE;i++) h_InB[i] = (float)(rand()%10)/10.0f; 
 
   printf("InA: "); for(i=0;i<SIZE;i++) printf(" %.2f",h_InA[i]); printf("\n");
   printf("InB: "); for(i=0;i<SIZE;i++) printf(" %.2f",h_InB[i]); printf("\n");
-
-  float *h_Out;
-  h_Out = (float*)malloc(sizeof(float)*SIZE);
 
   for(i=0;i<SIZE;i++) 
     h_Out[i] = h_InA[i]+ h_InB[i];
