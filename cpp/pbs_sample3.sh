c2d22a9ddca00b4143e85713541a6726b1bf16af
Tomoya Takiwaki <takiwaki.tomoya@nao.ac.jp>
2021-10-25 08:37:55 +0900

    update

--- cpp/pbs_sample3.sh
+++ cpp/pbs_sample3.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 #PBS -q dgx-full
-#PBS -N job_sample2
+#PBS -N job_sample3
 #PBS -l walltime=00:00:60
 #PBS -l select=1:ngpus=1:ncpus=1
 module load cuda-toolkit/11.0
 cd $PBS_O_WORKDIR
-./sample2.gpux > sample2.gpulog
-./sample2.cpux > sample2.cpulog
+./sample3.gpux > sample3.gpulog
+./sample3.cpux > sample3.cpulog
