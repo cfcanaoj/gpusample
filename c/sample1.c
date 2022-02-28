b663eadaca4b8d9ba2dfdbc4265efceceac904b6
Tomoya Takiwaki <takiwaki.tomoya@nao.ac.jp>
2022-02-28 11:00:30 +0900

    debug

--- c/sample1.c
+++ c/sample1.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 
 #define SIZE 256
+#define ishow 16
 
 int main(int argc, char**argv){
   int i;
@@ -15,8 +16,8 @@ int main(int argc, char**argv){
   for(i=0;i<SIZE;i++) h_InA[i] = (float)(rand()%10)/10.0f;
   for(i=0;i<SIZE;i++) h_InB[i] = (float)(rand()%10)/10.0f; 
 
-  printf("InA: "); for(i=0;i<SIZE;i++) printf(" %.2f",h_InA[i]); printf("\n");
-  printf("InB: "); for(i=0;i<SIZE;i++) printf(" %.2f",h_InB[i]); printf("\n");
+  printf("InA: "); for(i=0;i<ishow;i++) printf(" %.2f",h_InA[i]); printf("\n");
+  printf("InB: "); for(i=0;i<ishow;i++) printf(" %.2f",h_InB[i]); printf("\n");
 
   float *h_Out;
   h_Out = (float*)malloc(sizeof(float)*SIZE);
@@ -24,7 +25,7 @@ int main(int argc, char**argv){
   for(i=0;i<SIZE;i++) 
     h_Out[i] = h_InA[i]+ h_InB[i];
   
-  printf("Out: "); for(i=0;i<SIZE;i++) printf(" %.2f",h_Out[i]); printf("\n");
+  printf("Out: "); for(i=0;i<ishow;i++) printf(" %.2f",h_Out[i]); printf("\n");
   
   return 0;
 }
